b86721a5260d7729e0bfcab1dd31618a349d2b2d
Gian Ilchechen <89846332+gianilch@users.noreply.github.com>
2026-03-23 08:43:16 -0300

    refactor: remove express-async-errors dependency and update error handling
    - Removed the express-async-errors package from package.json and yarn.lock.
    - Updated app.ts to handle asynchronous errors without the express-async-errors middleware.
    - Cleaned up code formatting for consistency.

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,38 +1,38 @@
-import { AppError } from "@shared/errors/AppError";
-import "dotenv/config";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import "reflect-metadata";
-import swaggerUi from "swagger-ui-express";
-import upload from "../../../config/upload";
-import swaggerFile from "../../../swagger.json";
-import "../../container";
-import createConnection from "../typeorm";
-import { router } from "./routes";
+import { AppError } from "@shared/errors/AppError"
+import "dotenv/config"
+import express, { NextFunction, Request, Response } from "express"
+import "reflect-metadata"
+import swaggerUi from "swagger-ui-express"
+import upload from "../../../config/upload"
+import swaggerFile from "../../../swagger.json"
+import "../../container"
+import createConnection from "../typeorm"
+import { router } from "./routes"
 
-createConnection();
-const app = express();
+createConnection()
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-app.use("/avatars", express.static(`${upload.tmpFolder}/avatars`));
-app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+app.use("/avatars", express.static(`${upload.tmpFolder}/avatars`))
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
-app.use(router);
+app.use(router)
 
+// Middleware para capturar erros assíncronos não capturados
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
+      return response.status(err.statusCode).json({ message: err.message })
     }
 
     return response.status(500).json({
       status: "error",
       message: `Internal server error - ${err.message}`,
-    });
-  }
-);
+    })
+  },
+)
 
-export { app };
+export { app }
